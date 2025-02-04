54a0b2cb84d272064a75f92a5e93d282ad47260f
qousai <154825404+Qousai0@users.noreply.github.com>
2025-02-04 14:02:15 +0300

    Task 3 4/2/2025  2:02 PM listview example

--- lib/tasks/task3.dart
+++ lib/tasks/task3.dart
@@ -5,6 +5,27 @@ class Task3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  ListViewExample();
+  }
+}
+
+class ListViewExample extends StatelessWidget {
+  ListViewExample({super.key});
+  final products = List.generate(100, (index) => 'Product $index');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Example'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) => ListTile(
+          tileColor: (index % 2 == 0) ? Colors.grey[200] : Colors.white,
+          title: Text('Product $index'),
+        ),
+      ),
+    );
   }
 }
